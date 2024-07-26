3910adbdf1b607e0aba1e9ee79cce01291352621
Godwin Cameron <200109@virtualwindow.co.za>
2024-07-26 10:42:14 +0200

    Xcode Project creation

---
+++ PaparazziWalk/PaparazziWalk/PaparazziWalkApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaparazziWalkApp.swift
+//  PaparazziWalk
+//
+//  Created by Shiro NoKage on 2024/07/26.
+//
+
+import SwiftUI
+
+@main
+struct PaparazziWalkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
